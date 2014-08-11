fdc500525b622f00295380274b3c3d11d18a25af
Arjun S Nath <arjun.sn7c8@gmail.com>
2014-08-11 23:11:59 +0530

    Added code to open profiles of friends who have not send messages

--- get_message_non_senders.py
+++ get_message_non_senders.py
@@ -1,4 +1,8 @@
+#Scans the files storing friends ids and ids of message senders and opens the profile of non-senders to unfriend if needed
+
 import helper
+
+friends_names = {};
 
 def get_non_senders():
   non_senders = [];
@@ -6,7 +10,11 @@ def get_non_senders():
   senders = [];
   f = open("friends_ids.txt", "r");
   for line in f:
-    id1 = (line.split())[0];
+    nameid = line.split();
+    id1 = nameid[0];
+    friends_names[id1] = nameid[1];
+    if len(nameid) >= 3:
+      friends_names[id1] = friends_names[id1] + " " + nameid[2];
     friends.append(id1);
   f.close();
   f = open("message_senders.txt", "r");
@@ -18,6 +26,14 @@ def get_non_senders():
       non_senders.append(id);
   return non_senders;
 
+def open_non_senders():
+  non_senders = get_non_senders();
+  for i in non_senders:
+    response = raw_input("Do you consider unfriending " + friends_names[i] + "? (Y/N): ");
+    if response is "Y" or response is 'y':
+      helper.open_browser(i);
+      enter = raw_input("Press Enter when you are done: ");
+    else:
+      continue;
 
-
-open_browser();
+open_non_senders();
