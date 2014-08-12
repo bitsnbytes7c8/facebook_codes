6c7aa60ee092fa8102a32c6dd18e6ad1e2b54522
Arjun S Nath <arjun.sn7c8@gmail.com>
2014-08-12 16:12:55 +0530

    Created folder for this app

---
+++ find_message_non_senders/friends_list.py
@@ -0,0 +1,16 @@
+#Writes all your friends names and ids to a file named friends_ids.txt
+
+import urllib
+import facebook
+import access_token
+
+def get_friends_list(token):
+  graph = facebook.GraphAPI(token);
+  profile = graph.get_object("me");
+  friends = graph.get_connections("me", "friends")
+  friends_list = [friend['id'] for friend in friends['data']]
+  friends_name = [friend['name'] for friend in friends['data']]
+
+  f = open("friends_ids.txt", "w");
+  for id,name in zip(friends_list, friends_name): 
+    f.write((str(id) + " " + name+ "\n").encode('utf-8'));
---
+++ find_message_non_senders/helper.py
@@ -0,0 +1,30 @@
+#Contains helper functions for querying used by other programs
+
+import json
+import urllib
+import webbrowser
+
+def get_fql_json(query, access_token):
+  params = urllib.urlencode({'q':query, 'access_token':access_token});
+  url = "https://graph.facebook.com/fql?"+params;
+  print 'Querying: ' + url
+  data = urllib.urlopen(url).read();
+  rjson = json.loads(data);
+  return rjson;
+
+def get_name_from_id(uid, token):
+  query = "SELECT name FROM user WHERE uid="+str(uid);
+  name_json = get_fql_json(query, token);
+  name = name_json['data'];
+  for n in name:
+    return n['name'];
+
+def get_detail_from_id(detail, uid, token):
+  query = "SELECT" + detail + "FROM user WHERE uid="+str(uid);
+  detail_json = get_fql_json(query, token);
+  detail = detail_json['data'];
+  for d in detail:
+    return d[detail];
+
+def open_browser(id):
+  webbrowser.open('http://facebook.com/' + id);
